74a1e3977eba4afe5ac908aad1124339248a2c69
infyleaf <romigg@hotmail.fi>
2021-10-13 22:19:41 +0300

    toka osa security

--- src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
@@ -13,6 +13,9 @@ import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
 import hh.swd20.Bookstore.domain.Category;
 import hh.swd20.Bookstore.domain.CategoryRepository;
+import hh.swd20.Bookstore.domain.User;
+import hh.swd20.Bookstore.domain.UserRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -23,7 +26,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookTest(BookRepository repository, CategoryRepository catrep) {
+	public CommandLineRunner bookTest(BookRepository repository, CategoryRepository catrep, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 
@@ -37,6 +40,12 @@ public class BookstoreApplication {
 			repository.save(new Book("Title", "Author", 2005, "ISBN", 10, cat1));
 
 			repository.save(new Book("Art of code", "Antti Pori", 2011, "192.178.10.36", 101, cat2));
+			
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
 				log.info(book.toString());
